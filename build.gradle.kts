d020a15c86d7b463aa5701c9ef534364120e4999
asaad <abed.saad.securealm@gmail.com>
2021-01-31 20:27:26 +0200

    remove swig

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ buildscript {
         jcenter()
     }
     dependencies {
+        classpath("com.github.dcendents:android-maven-plugin:1.2")
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
     }
